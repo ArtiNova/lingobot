03c4ae017b4143d147786d0323f0586a4a1b1f7c
Amruth-github <79594127+Amruth-github@users.noreply.github.com>
2023-11-20 10:50:34 +0530

    serve script

---
+++ serve.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const helmet = require('helmet')
+
+app.use(helmet())
+
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
